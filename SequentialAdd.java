b68482fea5ab09e95a86efb4437b9417c54f95af
blessedchitamba <blessedbrighton@live.com>
2018-09-09 21:26:15 +0200

    now computes the total sunlight for each tree and prints out, and the average for all trees too

--- SequentialAdd.java
+++ SequentialAdd.java
@@ -13,6 +13,9 @@ public class SequentialAdd {
    private static int xTerrainSize;
    private static int yTerrainSize;
    private static int counter;
+   private static int numTrees;
+   private static int treeXindex, treeYindex, extension;
+   private static float treeTotal, grandTotal;
    
    public static void main(String[] args) throws FileNotFoundException {
       File input = new File(inputFileName);
@@ -23,20 +26,59 @@ public class SequentialAdd {
             
             //load the terrain matrix
             terrain = new float[xTerrainSize][yTerrainSize];
-            //scanner.nextLine();
-            counter = 0;
             for (int i=0; i<yTerrainSize; i++){
                for (int j=0; j<xTerrainSize; j++) {
                   terrain[i][j] = scanner.nextFloat();
                }
             }   
             
-            //test
-            for (int i=0; i<yTerrainSize; i++){
-               for (int j=0; j<xTerrainSize; j++) {
-                  System.out.print(terrain[i][j]+" ");
+            //deal with the trees now. take number of trees
+            numTrees = scanner.nextInt();
+            
+            //calculate average sunlight for each tree
+            counter = 1;
+            treeTotal = 0;
+            grandTotal = 0;
+            
+            while(counter<=numTrees) {
+               treeXindex = scanner.nextInt();
+               treeYindex = scanner.nextInt();
+               extension = scanner.nextInt();
+               treeTotal = 0;
+               
+               //deal with the 4 different cases of trees extending beyond the terrain size
+               if(treeXindex+extension < xTerrainSize && treeYindex+extension < yTerrainSize) {
+                  for(int i=treeYindex; i < treeYindex+extension; i++){
+                    for(int j=treeXindex; j < treeXindex+extension; j++){ 
+                       treeTotal += terrain[i][j];
+                       grandTotal += terrain[i][j];
+                    }
+                  }
+               } else if (treeXindex+extension > xTerrainSize && treeYindex+extension > yTerrainSize) {
+                    for(int i=treeYindex; i < yTerrainSize; i++){
+                       for(int j=treeXindex; j < xTerrainSize; j++){ 
+                          treeTotal += terrain[i][j];
+                          grandTotal += terrain[i][j];
+                       }
+                  }
+               } else if (treeXindex+extension > xTerrainSize) {
+                     for(int i=treeYindex; i < treeYindex+extension; i++){
+                       for(int j=treeXindex; j < xTerrainSize; j++){ 
+                          treeTotal += terrain[i][j];
+                          grandTotal += terrain[i][j];
+                       }
+                  }
+               } else {
+                     for(int i=treeYindex; i < yTerrainSize; i++){
+                       for(int j=treeXindex; j < treeXindex+extension; j++){ 
+                          treeTotal += terrain[i][j];
+                          grandTotal += terrain[i][j];
+                       }
+                  }
                }
-               System.out.println();
-            }
-      }
+
+               System.out.println("Tree "+counter+": "+treeTotal+" hours of sunlight.");
+               counter++;
+            } //end of while loop
+      } //end of main method
 }
